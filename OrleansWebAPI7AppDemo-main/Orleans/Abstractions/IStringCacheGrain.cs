ccb630a16ecacae905dfb920142ba779d4f806f6
kossygraph <104184767+kossygraph@users.noreply.github.com>
2023-09-29 13:51:47 +0900

    Add files via upload

---
+++ OrleansWebAPI7AppDemo-main/Orleans/Abstractions/IStringCacheGrain.cs
@@ -0,0 +1,8 @@
+﻿namespace OrleansWebAPI7AppDemo.Orleans.Abstractions
+{
+    public interface IStringCacheGrain : IGrainWithStringKey
+    {
+        Task SetValue(string value);
+        Task<string> GetValue();
+    }
+}
